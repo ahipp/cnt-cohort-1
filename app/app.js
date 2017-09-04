8eb504f96245d3f6dd8ea6407178112dfc5bb5d3
ahipp <hipp.adam@gmail.com>
2017-09-04 15:54:44 -0500

    Add JSON data base with first pass at reading scores from CSV. Add bootstrap and some layout work, layout nowhere near done yet.

--- app/app.js
+++ app/app.js
@@ -8,24 +8,30 @@ app.set('view engine', 'ejs');
 
 app.get('/property-details', function(req, res) {
     console.log('Property details request made. Address: ' + req.query.address); 
-    if (req.query.address) {
-		// Hacking the search to find a unique part of the search string for matching
-		// the demo files. 
-		var csvFileName = '';
-		if (req.query.address.toLowerCase().indexOf('fake') > 0) {
-			csvFileName = '123FakeStreet.csv'; 
-		}
-		var csvFile = fs.readFileSync('./app/datafiles/' + csvFileName);
+	var propertyAddress = "";
+    switch (req.query.address) {
+		case '123 Fake Street': propertyAddress = '123FakeStreet'; break;
+		default: propertyAddress = ''; 
+	}
+	
+	if (propertyAddress.length)
+	{
+		var pageJSON = require('./datafiles/' + propertyAddress + '.json');
+		var csvFile = fs.readFileSync('./app/datafiles/' + propertyAddress + 'Scores.csv');
+				
 		if (csvFile) {
 			var csvString = csvFile.toString();
+			var scoresJSON = {};
+			var label = '';
+			var attrVal = '';
+			csvString.split('\r\n').forEach(function(row) {
+				scoresJSON[row.split(',')[0].replace(' ','').toLowerCase()] = row.split(',')[1];
+			});
+		
+			pageJSON['scores'] = scoresJSON;
 		}
-		var pageJSON = {};
-		var label = '';
-		var attrVal = '';
-		csvString.split('\r\n').forEach(function(row) {
-			pageJSON[row.split(',')[0].replace(' ','').toLowerCase()] = row.split(',')[1];
-		});
 			
+		console.log(pageJSON);
 		res.render('../app/views/pages/property-details', pageJSON);
 	}
 	else {res.status(500).send('No property matches that address!')}
